92670edc13c8a5fe4a06d7742b216c2c649e31e3
ariel <ariel@LAPTOP-H2SGEEL2>
2023-03-08 08:45:48 -0500

    de prueba

--- src/centro_comercial/CENTRO_COMERCIAL.java
+++ src/centro_comercial/CENTRO_COMERCIAL.java
@@ -6,6 +6,7 @@ public class CENTRO_COMERCIAL {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("xxsxsxsxsxs");
         // TODO code application logic here
     }
     
